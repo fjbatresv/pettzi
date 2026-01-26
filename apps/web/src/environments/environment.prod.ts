93987975d2f2c508b77d5facaf6e408b5f04cd45
Javier Batres <fjbatresv@gmail.com>
2026-01-25 21:47:40 -0600

    Final fixes for deploy action

--- apps/web/src/environments/environment.prod.ts
+++ apps/web/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ export const environment = {
   cognitoDomain: 'https://us-east-1xx_hce2yn2.auth.us-east-1.amazoncognito.com',
   cognitoClientId: '1hhkaipbk6vt8i1nagm643br5l',
   cognitoRedirectUri: 'https://app.pettzi.net',
-  sentryDsn: 'https://0f54471497469300404fe6a6337f2431@o4510469539561472.ingest.us.sentry.io/4510772673118208',
-  sentryRelease: 'beta',
+  sentryDsn: '',
+  sentryRelease: '',
   sentryTracesSampleRate: 0.1,
   sentryProfilesSampleRate: 0.05,
 };
